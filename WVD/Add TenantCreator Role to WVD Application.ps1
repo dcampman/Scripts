e7f76504a7b9f0723e6147de9f18b44cfe5de360
dcampman <58568576+dcampman@users.noreply.github.com>
2020-03-30 10:58:33 -0400

    update

--- WVD/Add TenantCreator Role to WVD Application.ps1
+++
@@ -1,8 +0,0 @@
-﻿Connect-AzureAD
-
-$upn = Read-Host "Please enter the user account to add"
-$user = Get-AzureADUser -ObjectID $upn
-$appID = Get-AzureADServicePrincipal -Filter "displayname eq 'Windows Virtual Desktop'"
-$appRole = $appID.AppRoles | Where-Object {$_.DisplayName -eq "TenantCreator"}
-
-New-AzureADUserAppRoleAssignment -ObjectId $user.ObjectId -PrincipalId $user.ObjectId -ResourceId $appID.ObjectId -Id $appRole.Id
