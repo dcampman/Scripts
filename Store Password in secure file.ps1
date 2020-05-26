ec2facdbe99ed1f0c48d2664cd7a0ebe356073cf
dcampman <58568576+dcampman@users.noreply.github.com>
2020-05-26 17:05:35 -0400

    xgbn
    fdsgth

--- Store Password in secure file.ps1
+++
@@ -1,8 +0,0 @@
-﻿$CertName = "PSCert"
-
-#New-SelfSignedCertificate -DnsName $CertName -CertStoreLocation "Cert:\LocalMachine\My" -KeyUsage KeyEncipherment,DataEncipherment, KeyAgreement -Type DocumentEncryptionCert
-
-$Keypath = "c:\temp\SecureAADPassword.txt"
-$Password = Read-host "Please enter your password"
-
-$password | Protect-CmsMessage -To cn=$CertName -OutFile $Keypath
